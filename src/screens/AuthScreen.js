2bda3d1e943baf8fa7d763385d0ad42b671fc3db
whitzhu <whitzhu@gmail.com>
2018-07-08 15:08:34 -0700

    add basic auth input fields

--- src/screens/AuthScreen.js
+++ src/screens/AuthScreen.js
@@ -1,11 +1,42 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, TextInput, Button } from 'react-native';
 
 export default class AuthScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      authStateSignIn: true,
+      password: '',
+      email: '',
+    };
+  }
+
+  handleAuthStateChange = () => {
+    this.setState({ authStateSignIn: !this.state.authStateSignIn });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Auth Screen</Text>
+        <TextInput
+          autoFocus
+          placeholder="email"
+          style={styles.input}
+          onChangeText={(text) => this.setState({ email: text })} />
+        <TextInput
+          secureTextEntry
+          placeholder="password"
+          style={styles.input}
+          onChangeText={(text) => this.setState({ password: text })} />
+        <Button
+          title={ this.state.authStateSignIn ? 'Log In' : 'Sign Up' }
+          onPress={() => console.log('Button pressed')}
+        />
+        <Button
+          title={ this.state.authStateSignIn ? 'Sign Up' : 'Log In' }
+          onPress={this.handleAuthStateChange}
+        />
       </View>
     );
   }
@@ -14,7 +45,14 @@ export default class AuthScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    height: 50,
+    width: 300,
+    padding: 10,
+    marginVertical: 10,
+    alignSelf: 'stretch',
   },
 });
