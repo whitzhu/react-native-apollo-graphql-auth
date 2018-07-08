122d6d8e5a14361bc7cc31f1ca60d075bb9feb5f
whitzhu <whitzhu@gmail.com>
2018-07-08 14:18:08 -0700

    add ApolloClient

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  uri: 'https://api.graph.cool/simple/v1/cjjdbv8l01dov0158mp7le2b1',
+});
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <ApolloProvider client={client}>
+        <View style={styles.container}>
+          <Text>React Native Apollo Client</Text>
+        </View>
+      </ApolloProvider>
     );
   }
 }
