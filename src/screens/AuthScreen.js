4de30feac1d80ec463a2a8f661b41a121e7040dd
whitzhu <whitzhu@gmail.com>
2018-07-08 14:42:20 -0700

    add basic auth and home screen

---
+++ src/screens/AuthScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default class AuthScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Auth Screen</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
